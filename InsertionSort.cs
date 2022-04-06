ae30a0bb19195605dece3ea8735bbc5d58ef4b78
EissenLu <lukas.eissen@yahoo.de>
2022-04-06 15:59:06 +0200

    StartUp after Reset

---
+++ InsertionSort.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace SortiertAlgorithmen
+{
+    public class InsertionSort
+    {
+        private int[] _input;
+
+        public InsertionSort(int[] input)
+        {
+            this._input = input;
+        }
+
+        public void SortArray()
+        {
+            Console.WriteLine("Not yet Implemented");
+        }
+
+        public override string ToString()
+        {
+            return "Not yet implemented";
+        }
+    }
+}
